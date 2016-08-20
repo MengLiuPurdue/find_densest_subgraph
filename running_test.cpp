ac96bd5db756a30814ec432029dd36ceb8f466cb
David Gleich <dgleich@purdue.edu>
2016-08-20 13:21:45 -0400

    Updated with set output

--- running_test.cpp
+++ running_test.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sstream>
+#include <inttypes.h>
 
 using namespace std;
 
@@ -10,10 +11,9 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     FILE *rptr = fopen(argv[1], "r");
-    if(rptr == NULL)
-    {
-	fprintf(stderr, "Invalid input file!\n");
-	return EXIT_FAILURE;
+    if(rptr == NULL) {
+        fprintf(stderr, "Invalid input file!\n");
+        return EXIT_FAILURE;
     }
 
     /*read the content from the input file to a stringstream*/
@@ -21,14 +21,14 @@ int main(int argc, char* argv[])
     int64_t fsize;
     fsize = ftell(rptr);
     char *read_file = (char *)malloc(sizeof(char) * fsize);
-    if(read_file == NULL)
-    {
-	fprintf(stderr, "malloc failing!\n");
-	fclose(rptr);
-	return EXIT_FAILURE;
+    if(read_file == NULL) {
+        fprintf(stderr, "malloc failing!\n");
+        fclose(rptr);
+        return EXIT_FAILURE;
     }
     fseek(rptr, 0, SEEK_SET);
     fread(read_file, sizeof(char), fsize, rptr);
+    
     stringstream ss;
     ss << read_file;
     free(read_file);
@@ -44,16 +44,30 @@ int main(int argc, char* argv[])
     ej = (int64_t *)malloc(sizeof(int64_t) * m);
     w = (double *)malloc(sizeof(double) * m);
     int64_t i;
-    for(i = 0; i < m; i ++)
-    {
-	ss >> ei[i];
-	ss >> ej[i];
-	ss >> w[i];
+    for(i = 0; i < m; i ++) {
+        ss >> ei[i];
+        ss >> ej[i];
+        ss >> w[i];
     }
+    
+    // TODO check symmetric
+    // TODO check for diagonal edges
+    // TODO check for negative weights
+    // TODO validate input
+    // TODO check for repeated edges
+    
+    fprintf(stderr, "loaded file %s : %12" PRId64 " nodes   %12" PRId64 " edges\n",
+        argv[1], n, m);
 
     double ret;
     int64_t *output = (int64_t *)malloc(sizeof(int64_t) * n);
-    size_t outputlen;
+    size_t outputlen = n;
     ret = densest_subgraph(n, m, ei, ej, w, output, &outputlen);
     cout << ret << endl;
+    cout << outputlen << endl;
+    for (i = 0; i < outputlen; ++i) {
+        cout << output[i] << endl;
+    }
+    
+    return 0;
 }
