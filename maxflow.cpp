3cb9c333845b502f919a51bc17eaf855c2dd3afb
Meng Liu <liumeng19952012@outlook.com>
2016-08-22 17:47:19 -0400

    Add files via upload

--- maxflow.cpp
+++ maxflow.cpp
@@ -28,16 +28,16 @@ bool dinic_bfs(int64_t nverts, int64_t src, int64_t dest, int64_t *dist, int64_t
     Q[en ++] = src;
     while(st < en) 
     {
-    u = Q[st ++];
-    for(i = fin[u]; i >= 0; i = next[i]) 
-    {
-        v = to[i];
-        if(flow[i] < cap[i] && dist[v] == -1) 
+        u = Q[st ++];
+        for(i = fin[u]; i >= 0; i = next[i]) 
         {
-        dist[v] = dist[u] +1;
-        Q[en ++] = v;
+            v = to[i];
+            if(flow[i] < cap[i] && dist[v] == -1) 
+            {
+                dist[v] = dist[u] +1;
+                Q[en ++] = v;
+            }
         }
-    }
     }
     return dist[dest] != -1;
 }
@@ -49,21 +49,21 @@ double dinic_dfs(int64_t u, double fl, int64_t src, int64_t dest, int64_t *pro, 
     double df;
     for(int64_t &e=pro[u]; e >= 0; e = next[e]) 
     {
-    v = to[e];
-    if(flow[e] < cap[e] && dist[v] == dist[u] + 1) 
-    {
-        if(u == src || (cap[e] - flow[e]) <= fl)
+        v = to[e];
+        if(flow[e] < cap[e] && dist[v] == dist[u] + 1) 
         {
-        fl = cap[e] - flow[e];
+            if(u == src || (cap[e] - flow[e]) <= fl)
+            {
+                fl = cap[e] - flow[e];
+            }
+            df = dinic_dfs(v, fl, src, dest, pro, next, to, dist, cap, flow);
+            if(df>0) 
+            {
+                flow[e] += df;
+                flow[e^1] -= df;
+                return df;
+            }
         }
-        df = dinic_dfs(v, fl, src, dest, pro, next, to, dist, cap, flow);
-        if(df>0) 
-        {
-        flow[e] += df;
-        flow[e^1] -= df;
-        return df;
-        }
-    }
     }
     return 0;
 }
@@ -74,11 +74,11 @@ void find_cut(int64_t u, int64_t *cut, int64_t *another_pro, int64_t *next, int6
     cut[u] = 1;
     for(int64_t &e = another_pro[u]; e >= 0; e = next[e])
     {
-    int64_t v = to[e];
-    if(flow[e] < cap[e] && cut[v] == 0)
-    {
-        find_cut(v, cut, another_pro, next, to, flow, cap);
-    }
+        int64_t v = to[e];
+        if(flow[e] < cap[e] && cut[v] == 0)
+        {
+            find_cut(v, cut, another_pro, next, to, flow, cap);
+        }
     }
 }
 
@@ -91,25 +91,25 @@ double max_flow(double (*edges_info)[3], int64_t nverts, int64_t nedges, int64_t
     long nEdge = 0;
     for(i = 0; i < nedges; i ++)
     {
-    new_edge(edges_info[i][0], edges_info[i][1], edges_info[i][2], to, cap, flow, next, fin, &nEdge);
+        new_edge(edges_info[i][0], edges_info[i][1], edges_info[i][2], to, cap, flow, next, fin, &nEdge);
     }
 
     double ret = 0;
     double df;
     while(dinic_bfs(nverts, src, dest, dist, Q, fin, next, to, flow, cap)) 
     {
-    for(i = 0; i < nverts; i++) 
-    {
-        pro[i] = fin[i];
-        another_pro[i] = fin[i];
-        pro3[i] = fin[i];
-    }
-    while(true) 
-    {
-        df = dinic_dfs(src, 0, src, dest, pro, next, to, dist, cap, flow);
-        if(df) ret += df;
-        else break;
-    }
+        for(i = 0; i < nverts; i++) 
+        {
+            pro[i] = fin[i];
+            another_pro[i] = fin[i];
+            pro3[i] = fin[i];
+        }
+        while(true) 
+        {
+            df = dinic_dfs(src, 0, src, dest, pro, next, to, dist, cap, flow);
+            if(df) ret += df;
+            else break;
+        }
     }
     find_cut(src, cut, another_pro, next, to, flow, cap);
     return ret;
