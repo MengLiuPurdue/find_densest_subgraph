3ca212531bf4a1493c1381f9ba09364311aa0f84
Meng Liu <liumeng19952012@outlook.com>
2016-09-10 18:56:09 -0400

    Add files via upload

--- small_graph_test/max_density.m
+++ small_graph_test/max_density.m
@@ -1,14 +1,11 @@
-%find the subgraph with the maximum density in an undirected graph through brutal search
-%the undirected graph is represented by an adjcent matrix A
-%density is the maximum density and cut is the corresponding subgraph
-
 function [density,cut]=max_density(A)
 density=0;
-for i=0:63
+[n,n]=size(A);
+for i=0:(2^n-1)
    temp=0;
-   a=bitget(i,6:-1:1);
-   for j=1:6
-       for k=1:6
+   a=bitget(i,n:-1:1);
+   for j=1:n
+       for k=1:n
            if a(j)*a(k)*A(j,k)~=0
                temp=temp+A(j,k);
            end
@@ -22,7 +19,7 @@ for i=0:63
    end
 end
 j=1;
-for i=1:6
+for i=1:n
     if max_cut(i)==1
         cut(j,1)=i;
         j=j+1;
